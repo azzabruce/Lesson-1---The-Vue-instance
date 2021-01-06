27b9e73a29a978c4b09ebfe1e51770737d8efedb
Azza Bruce <azzabbruce@gmail.com>
2021-01-06 12:06:48 -0500

    First commit to Vue.js master class practice files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        // option titles
+        title1: 'Workouts',
+        title2: 'My Progress',
+        title3: 'My nutrition',
+        title4: 'My Sleep',
+        title5: 'My Heart Rate',
+        title6: 'Help',
+        //links
+        home: 'index.html'
+
+        
+    }
+})
+
+//product description
